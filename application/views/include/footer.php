1b2fa7c54b6c5d271de491a963ab7a4b43ad6ac2
priyank-m <your_github_email>
2022-03-21 17:55:13 +0530

    first commit

---
+++ application/views/include/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">  
+        <p>Copyright (c) 2016 codeigniter.com All Rights Reserved</p>  
+    </div>  
+  
+</div>  
+</body>  
+</html>  
---
+++ application/views/include/nav.php
@@ -0,0 +1,10 @@
+<body>  
+<div id="container">  
+    <div id="nav">  
+        <ul>  
+            <li><a href="#">Home</a></li>  
+            <li><a href="#">About Us</a></li>  
+            <li><a href="#">Contact Us</a></li>  
+            <li><a href="#">Career</a></li>  
+        </ul>  
+    </div>  
