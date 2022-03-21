fbeb6a8bd135c45174e00e61925aa51cb8e4af55
priyank-m <your_github_email>
2022-03-21 18:47:02 +0530

    21-03-2020 06:46

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('hello_world');
+		$data['pageName']= 'home';
+ 		$this->load->view('start',$data);
 	}
 }
--- application/views/include/footer.php
+++
@@ -1,7 +0,0 @@
-<div id="footer">  
-        <p>Copyright (c) 2016 codeigniter.com All Rights Reserved</p>  
-    </div>  
-  
-</div>  
-</body>  
-</html>  
